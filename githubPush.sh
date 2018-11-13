a079c9e05a9db060694ee04820fc35d5525ce1b9
nvdinh185 <nvdinh0905671805@gmail.com>
2018-11-13 11:02:04 +0700

    push to heroku

---
+++ githubPush.sh
@@ -0,0 +1,4 @@
+git add .
+git commit -m "push to git hub"
+#git remote add origin https://github.com/nvdinh185/reactive-demo.git
+git push -u origin master
---
+++ herokuPush.sh
@@ -0,0 +1,4 @@
+git add .
+git commit -am "push to heroku"
+git push heroku master
+heroku open
