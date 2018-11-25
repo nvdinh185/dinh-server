daa568bc3a1641cdfe6be188122e0b137204a566
nvdinh185 <nvdinh0905671805@gmail.com>
2018-11-25 16:56:55 +0700

    push to heroku

--- pushToGit.sh
+++ pushToGit.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "push to git"
-git remote add origin https://github.com/nvdinh185/dinh-server-heroku.git
+#git remote add origin https://github.com/nvdinh185/dinh-server-heroku.git
 git push -u origin master
