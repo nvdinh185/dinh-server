262a2f31dddcd3edc588adc21167b3c24f684e63
nvdinh185 <nvdinh0905671805@gmail.com>
2019-01-04 16:47:37 +0700

    push to git

--- pushToGit.sh
+++ pushToGit.sh
@@ -1,5 +1,5 @@
-git init
+#git init
 git add .
 git commit -m "push to git"
-git remote add origin https://github.com/nvdinh185/dinh-server.git
+#git remote add origin https://github.com/nvdinh185/dinh-server.git
 git push -u origin master
