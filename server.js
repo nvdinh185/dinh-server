395e804eb61c07a0d4b6662b425605d2c0ad0055
nvdinh185 <nvdinh0766777123@gmail.com>
2022-06-06 09:01:48 +0700

    add new

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ var fs = require('fs');
 var url = require('url');
 
 // Create http server.
-var httpServer = http.createServer(function (req, resp) {
+var httpServer = http.createServer(function (req, res) {
 
     // Get client request url.
     var reqUrlString = req.url;
-
+    
     // Get client request path name.
     var pathName = url.parse(reqUrlString, true, false).pathname;
-
+    
     // If request login action.
-    //if ('/login' == pathName) {
+    // if ('/login' == pathName) {
         // Get request method.
         var method = req.method;
 
@@ -24,29 +24,28 @@ var httpServer = http.createServer(function (req, resp) {
             // Get all post data when receive data event.
             req.on('data', function (chunk) {
                 postData += chunk;
-
             });
 
             // When all request post data has been received.
             req.on('end', function () {
 
-                console.log("Client post data : " + postData);
+                // console.log("Client post data : " + postData);
 
                 // Parse the post data and get client sent username and password.
                 var postDataObject = JSON.parse(postData);
 
-                var userName = postDataObject.user_name;
+                var username = postDataObject.username;
 
                 var password = postDataObject.password;
 
                 /* Set Access-Control-Allow-Origin http header will fix No 'Access-Control-Allow-Origin' header is present on the requested resource error
                    when use XMLHttpRequest object to get this server page via ajax method. */
-                resp.writeHead(200, { 'Access-Control-Allow-Origin': '*' });
+                res.writeHead(200, { 'Access-Control-Allow-Origin': '*' });
 
-                if ('dinh' == userName && '1234' == password) {
-                    resp.end('{status: "ok", message: "ban da login thanh cong!"}');
+                if ('user1' == username && '123' == password) {
+                    res.end('{status: "ok", message: "ban da login thanh cong!"}');
                 } else {
-                    resp.end('{status: "nok", message: "khong thanh cong"}');
+                    res.end('{status: "nok", message: "khong thanh cong"}');
                 }
             })
         } else if ("GET" == method) {
@@ -62,18 +61,15 @@ var httpServer = http.createServer(function (req, resp) {
             fs.readFile(fileName, { encoding: 'utf-8', flag: 'r' }, function (error, data) {
 
                 if (!error) {
-                    resp.writeHead(200, { 'Access-Control-Allow-Origin': '*' });
-                    resp.end(data);
+                    res.writeHead(200, { 'Access-Control-Allow-Origin': '*' });
+                    res.end(data);
                 } else {
-                    resp.writeHead(404, { 'Access-Control-Allow-Origin': '*' });
-                    resp.end(JSON.stringify(error));
+                    res.writeHead(404, { 'Access-Control-Allow-Origin': '*' });
+                    res.end(JSON.stringify(error));
                 }
             });
         }
-    //}
+    // }
 });
 
-
-httpServer.listen(process.env.PORT || 8888);
-
-console.log("Server is started.");
+httpServer.listen(process.env.PORT || 4000, () => console.log("Server is started on port 4000..."));
